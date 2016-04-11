ca1e471a287a99704ef5b9690343fde934c5a491
Aitor Brazaola <aitor.bv@deusto.es>
2016-04-11 12:07:27 +0200

    Fixed order type constants.

--- auzonet/auzonetweb/views.py
+++ auzonet/auzonetweb/views.py
@@ -23,8 +23,8 @@ Here is where the POST and GET request are processed
 Auth API docs https://docs.djangoproject.com/en/1.9/topics/auth/default/
 Customizing the authentication on Django https://docs.djangoproject.com/en/1.9/topics/auth/customizing/
 """
-REQUEST_TYPE = 'Request'
-OFFER_TYPE = 'Offer'
+REQUEST_TYPE = 'R'
+OFFER_TYPE = 'O'
 KARMA_REWARD = 10
 STATUS_ACTIVE = 'A'
 STATUS_FINISHED = 'F'
