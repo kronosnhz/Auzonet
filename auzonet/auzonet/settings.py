310e8e41b80cbb1215d8630bbe486e83fe3db1c2
Aitor Brazaola <aitor.bv@deusto.es>
2016-04-28 12:55:25 +0200

    Fixed errors in the community selection form.

--- auzonet/auzonet/settings.py
+++ auzonet/auzonet/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 USE_X_FORWARDED_HOST = True
 FORCE_SCRIPT_NAME = '/auzonet'
 # END SERVER
-FAVICON_PATH = STATIC_URL + 'images/favicon/'
+FAVICON_PATH = STATIC_URL + 'images/favicon/favicon.ico'
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
